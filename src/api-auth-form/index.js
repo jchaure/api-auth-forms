644a9a9060368fa2826005bf805802bf09831143
Javier Chaure <javier.chaure@insided.com>
2017-01-16 17:39:25 +0100

    - Added api-auth-form input generation for type string fields - Added example DOM id

--- src/api-auth-form/index.js
+++ src/api-auth-form/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var $ = require('jquery');
+
 var ApiAuth = require('./../api-auth');
 
 var ApiAuthForm = function (options) {
@@ -8,11 +10,56 @@ var ApiAuthForm = function (options) {
     throw new Error('You must provide a customer Id');
   }
 
+  var self = this;
+
+  this.id = '#api-auth-form';
+
   this.apiAuth = new ApiAuth({
     customerId: options.customerId
   });
 
-  this.config = this.apiAuth.config.get();
+  this.apiAuth.strategies.get().done(function (strategies) {
+      strategies.forEach(function (strategy) {
+        self.createProviderForm(strategy);
+      })
+  });
+
+}
+
+ApiAuthForm.prototype.createProviderForm = function (strategy) {
+  $(this.id).append('<h2>'+ strategy.provider +'</h2>');
+
+  var form = document.createElement('form');
+
+  form.setAttribute('id', 'api-auth-form-' + strategy.provider);
+  form.setAttribute('method', 'POST');
+
+  this.createProviderFields(form, strategy);
+
+  var submit = document.createElement('input');
+  submit.setAttribute('type', 'submit');
+  submit.setAttribute('value', 'Save');
+
+  $(this.id).append(form);
+  $(form).append(submit);
+
+}
+
+ApiAuthForm.prototype.createProviderFields = function (form, strategy) {
+
+  strategy.fields.forEach(function (field) {
+    switch (field.type) {
+      case 'String':
+        var textField = document.createElement('input');
+
+        textField.setAttribute('type', 'text');
+        textField.setAttribute('name', field.name);
+        textField.setAttribute('value', field.value);
+
+        $(form).append(textField);
+      break;
+    }
+  });
 
 }
 
