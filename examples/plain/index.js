3fe2b8223c56b2bf8b201031a605d03a71abce60
luis.grande <luis.grande@insided.com>
2017-01-17 16:28:28 +0100

    fix naming in examples

--- examples/plain/index.js
+++ examples/plain/index.js
@@ -2,9 +2,9 @@
 
 var $ = require('jquery');
 
-var ApiAuthView = require('../../dist/api-auth-forms');
+var ApiAuthFormView = require('../../dist/api-auth-forms');
 
-new ApiAuthView({
+new ApiAuthFormView({
   $el: $('#api-auth-form'),
   baseUrl: 'http://192.168.99.100:3000',
   customerId: 'patata'
